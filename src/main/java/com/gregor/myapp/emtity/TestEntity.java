fa726d275e12a8a431316e2f71bee02fc27a4801
GregorGGL <gregor.gonzales.l@uni.pe>
2022-06-09 11:53:28 -0500

    Nombre: my-app

---
+++ src/main/java/com/gregor/myapp/emtity/TestEntity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.gregor.myapp.emtity;
+
+/**
+ * @author Gregor
+ *
+ */
+public class TestEntity {
+
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
